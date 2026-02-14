5ed846efe2c20bc320ff0b30e48dea5c3e7af453
Di10n <dilonchugh@gmail.com>
2026-02-14 01:37:56 -0800

    fix migrations

--- supabase/migrations/005_leaderboard_profiles_select.sql
+++
@@ -1,4 +0,0 @@
--- Allow approved users to read all profiles (for leaderboard)
-CREATE POLICY "Approved users can view all profiles for leaderboard"
-  ON public.profiles FOR SELECT
-  USING (public.is_approved());
